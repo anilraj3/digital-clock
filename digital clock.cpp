224a64dc4b565f996303ee14e5bc3719f8c4ccbd
anilraj3 <108816727+anilraj3@users.noreply.github.com>
2022-07-06 20:01:12 +0530

    Add files via upload

---
+++ digital clock.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	int h,m,s;
+	int d = 1000; // we add a delay of 1000 milliseconds and we will use it in the function sleep 
+	
+	printf("set time:\n");
+	scanf("%d %d %d",&h,&m,&s);
+	if(h>12||m>60||s>60)
+	{
+		printf("ERROR !\n");
+		
+	}
+	while(1)//this is an initiative loup and anything inside will repeat itself to infinity
+	{
+		s++;
+		if(s>59)
+	{
+		m++;
+		s=0;
+	}
+	if(m>59)
+	{
+		h++;
+		m=0;
+	}
+	if(h>12)
+	{
+	  h=1;
+	}
+	printf("\n clock:");
+	printf("\n %02d: %02d: %02d:",h,m,s);//this writes our times in this format 00:00:00
+}
+}
